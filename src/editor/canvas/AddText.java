8ed75f1c8b3b15c0d92d456dbf03f1b9c4f25000
voskanyan069 <voskanyan069@gmail.com>
2021-01-06 19:00:22 +0400

    added custom font in add text

--- src/editor/canvas/AddText.java
+++ src/editor/canvas/AddText.java
@@ -2,15 +2,17 @@ package editor.canvas;
 
 
 import editor.config.ChangeStage;
+import editor.config.Colors;
 import editor.controllers.GraphicEditorController;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import static editor.controllers.GraphicEditorController.graphicsContext;
 
 public class AddText {
     private final Button addTextBtn;
-    private static GraphicsContext graphicsContext;
 
     public AddText(Canvas canvas, Button addTextBtn) {
         this.addTextBtn = addTextBtn;
@@ -18,14 +20,13 @@ public class AddText {
     }
 
     public void openDialog() {
-        addTextBtn.setOnAction(actionEvent -> {
-            ChangeStage.changeStage(GraphicEditorController.class, "../ui/add_text_alert.fxml",
-                    "Add Text", new int[]{600, 400}, new int[]{600, 400}, false);
-        });
+        addTextBtn.setOnAction(actionEvent -> ChangeStage.changeStage(GraphicEditorController.class, "../ui/add_text_alert.fxml",
+                "Add Text", new int[]{600, 400}, new int[]{600, 400}, false));
     }
 
-    public static void addText(String text, double x, double y, Font font) {
-        graphicsContext.setFont(font);
-        graphicsContext.strokeText(text, x, y);
+    public static void addText(String text, double x, double y, String fontFamilyPath, int fontSize) {
+        graphicsContext.setFill(Colors.brushColor);
+        graphicsContext.fillText(text, x, y);
+        graphicsContext.setFont(Font.loadFont(fontFamilyPath, fontSize));
     }
 }
--- src/editor/controllers/AddTextAlertController.java
+++ src/editor/controllers/AddTextAlertController.java
@@ -6,7 +6,9 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class AddTextAlertController {
 
@@ -31,6 +33,8 @@ public class AddTextAlertController {
     private double x;
     private double y;
     private int fontSize;
+    private String fontFamilyPath;
+    private final FileChooser fileChooser = new FileChooser();
 
     @FXML
     void initialize() {
@@ -47,14 +51,23 @@ public class AddTextAlertController {
                 fontSize = 13;
             }
 
-            AddText.addText(addTextArea.getText(), x, y, new Font(fontSize));
+            AddText.addText(addTextArea.getText(), x, y, "file:" + fontFamilyPath, fontSize);
             ((Node) actionEvent.getSource()).getScene().getWindow().hide();
         });
     }
 
     private void changeFontFamily() {
         selectFontBtn.setOnAction(actionEvent -> {
+            FileChooser.ExtensionFilter extensionFilter =
+                    new FileChooser.ExtensionFilter("font family files (*.ttf)", "*.ttf");
+            fileChooser.getExtensionFilters().add(extensionFilter);
+            fileChooser.setTitle("Open font");
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+            File file = fileChooser.showOpenDialog(((Node) actionEvent.getSource()).getScene().getWindow());
 
+            if (file != null) {
+                fontFamilyPath = file.getAbsolutePath();
+            }
         });
     }
 
